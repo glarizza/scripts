b0622bb4962fa5ec12d1c876d5c5d5e451ee8c8c
Gary Larizza <gary@huronhs.com>
2011-02-10 11:04:13 -0500

    Added a sample script for importing into a hash

---
+++ ruby/hash.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+hash = {}
+
+if File.exist?("/users/glarizza/Desktop/testhash.txt")
+    File.readlines("/users/glarizza/Desktop/testhash.txt").each do |line|
+        newarray = line.split(",")
+        hash[newarray[0]] = newarray[1]
+        puts "hash[#{newarray[0]}] is #{hash[newarray[0]]}"
+    end
+end
+
+#  The contents of /users/glarizza/Desktop/testhash.txt is:
+#
+# test,1
+# this,2
+# is,3
+# a,4
+# test,5
+#
