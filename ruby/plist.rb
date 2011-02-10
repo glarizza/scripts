3aa7dc6a5057aa98716a7f323cc292e6a7b95756
Gary Larizza <gary@huronhs.com>
2011-02-10 16:14:03 -0500

    Minor changes to plist.rb script

--- ruby/plist.rb
+++ ruby/plist.rb
@@ -2,10 +2,12 @@
 require "osx/cocoa"
 require 'pp'
 
-myfile = 'com.huronhs.example.plist'
+# Create the plist and assign values
+myfile = '/Users/glarizza/Desktop/com.huronhs.example.plist'
 my_dict = OSX::NSMutableDictionary.dictionary
-my_dict['CFBundleVersion'] = '2.0.1'
-my_dict['count'] = '15'
-my_dict.writeToFile_atomically(myfile, true)
+my_dict['The_Color'] = 'blue'
+my_dict['count'] = 15
 
+# Output to File and print
+my_dict.writeToFile_atomically(myfile, true)
 pp my_dict
