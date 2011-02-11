c52618a2d5e8b5ffdfcd9784ed626b4e5d737355
Gary Larizza <gary@huronhs.com>
2011-02-11 15:39:43 -0500

    Added bitshift documentation

--- ruby/exitcode_check.rb
+++ ruby/exitcode_check.rb
@@ -2,6 +2,7 @@
 
 # This command will execute successfully
 system ("which /usr/local/bin/macruby > /dev/null 2>&1")
+# You need to bitshift 8 spaces to get to the actual exit code.
 puts "Macruby Exists!" if (($? >> 8) == 0)
 
 # This command will not
