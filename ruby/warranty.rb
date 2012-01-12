f402b098446d2ece92ea71ab38532efc8bd6601b
Gary Larizza <gary@puppetlabs.com>
2012-01-11 20:57:34 -0800

    Fix the ugly warranty script
    I dunno what the hell I was thinking, but I tried to parse JSON
    with a bunch of splits and regex...which was ugly as hell. Now the
    script ACTUALLY USES the JSON library to parse a JSON string (weird,
    huh?). Feel free to take this and run with it.

--- ruby/warranty.rb
+++ ruby/warranty.rb
@@ -2,37 +2,32 @@
 #
 # File:  Warranty.rb
 #
-# Decription: Contact's Apple's selfserve servers to capture warranty information
-#              about your product. Accepts arguments of machine serial numbers.
-
+# Decription:     Contact's Apple's selfserve servers to capture warranty
+#                 information about your product. Accepts arguments of
+#                 machine serial numbers.
+#
+# Author:         Gary Larizza
+# Last Modified:  1/11/2012
+# Why:            It was ugly
+#
 require 'open-uri'
 require 'openssl'
-
-# This is a complete hack to disregard SSL Cert validity for the Apple
-#  Selfserve site.  We had SSL errors as we're behind a proxy.  I'm
-#  open suggestions for doing it 'Less-Hacky.' You can delete this 
-#  code if your network does not have SSL issues with open-uri.
-module OpenSSL
-  module SSL
-    remove_const:VERIFY_PEER
-  end
-end
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+require 'json'
 
 def get_warranty(serial)
-  hash = {}
-  open('https://selfsolve.apple.com/warrantyChecker.do?sn=' + serial.upcase + '&country=USA') {|item|
-    item.each_line {|item|}
-    warranty_array = item.strip.split('"')
-    warranty_array.each {|array_item|
-      hash[array_item] = warranty_array[warranty_array.index(array_item) + 2] if array_item =~ /[A-Z][A-Z\d]+/
-    }
-    
-    puts "\nSerial Number:\t\t#{hash['SERIAL_ID']}\n"
-    puts "Product Decription:\t#{hash['PROD_DESCR']}\n"
-    puts "Purchase date:\t\t#{hash['PURCHASE_DATE'].gsub("-",".")}"
-    puts (!hash['COV_END_DATE'].empty?) ? "Coverage end:\t\t#{hash['COV_END_DATE'].gsub("-",".")}\n" : "Coverage end:\t\tEXPIRED\n"
-  }
+  warranty_data = {}
+  raw_data = open('https://selfsolve.apple.com/warrantyChecker.do?sn=' + serial.upcase + '&country=USA')
+  warranty_data = JSON.parse(raw_data.string[5..-2])
+
+  puts "\nSerial Number:\t\t#{warranty_data['SERIAL_ID']}\n"
+  puts "Product Decription:\t#{warranty_data['PROD_DESCR']}\n"
+  puts "Purchase date:\t\t#{warranty_data['PURCHASE_DATE'].gsub("-",".")}"
+
+  unless warranty_data['COV_END_DATE'].empty?
+    puts "Coverage end:\t\t#{warranty_data['COV_END_DATE'].gsub("-",".")}\n"
+  else
+    puts "Coverage end:\t\tEXPIRED\n"
+  end
 end
 
 if ARGV.size > 0 then
